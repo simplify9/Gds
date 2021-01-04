c66bdaf65ffc202e6eb108548ab7b27cdda5ebad
Samer Awajan <samer@simplify9.com>
2021-01-04 13:11:34 +0200

    chart

--- SW.Gds.Util/Importer.cs
+++ SW.Gds.Util/Importer.cs
@@ -1,27 +1,27 @@
-﻿using CsvHelper;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using CsvHelper;
+//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.IO;
+//using System.IO.Compression;
+//using System.Linq;
+//using System.Runtime.Serialization;
+//using System.Runtime.Serialization.Formatters.Binary;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SW.I18n.Fill
-{
-    public static partial class Importer
-    {
-        async public static Task FillCountriesAndCurrencies()
-        {
+//namespace SW.I18n.Fill
+//{
+//    public static partial class Importer
+//    {
+//        async public static Task FillCountriesAndCurrencies()
+//        {
 
 
-        }
-        async public static Task FillPnp()
-        {
+//        }
+//        async public static Task FillPnp()
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
